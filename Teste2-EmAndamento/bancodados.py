38ef75fdfd53aad6063a5eaabcf34a9946d7915f
Hélio <HelioJR780@Gmail.com>
2025-03-28 20:22:42 -0300

    dasfsdf

--- Teste2-EmAndamento/bancodados.py
+++ Teste2-EmAndamento/bancodados.py
@@ -1,23 +1,28 @@
-import pymysql
 import pandas as pd
-import sqlalchemy 
+from sqlalchemy import create_engine
 
-USUARIO = "seu_usuario"
-SENHA = "sua_senha"
+USER = "root"
+PASSWORD = "sua_senha"
 HOST = "localhost"
-BANCO = "seu_banco"
-ARQUIVO_CSV = "Relatorio_cadop.csv"
+DATABASE = "saude"
 
-conn = pymysql.connect(host=HOST, user=USUARIO, password=SENHA, database=BANCO)
-cursor = conn.cursor()
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}")
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS relatorio_cadop (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(100) NOT NULL,
-        idade INT,
-        cidade VARCHAR(100),
-        data_registro DATE
-    );
-''')
+csv_files = ["D:/VS-Code/IntuitiveCare/Teste2-EmAndamento/Arquivos/4T2023.csv", "D:/VS-Code/IntuitiveCare/Teste2-EmAndamento/Arquivos/4T2024.csv"]
 
+file = "D:/VS-Code/IntuitiveCare/Teste2-EmAndamento/Arquivos/4T2023.csv"
+df = pd.read_csv(file, encoding="utf-8", sep=";", parse_dates=["DATA"], on_bad_lines='warn')  # Aqui você aplica a solução
+
+for file in csv_files:
+    print(f"Importando: {file}")
+    
+    df["DATA"] = pd.to_datetime(df["DATA"], errors="coerce")
+    print(df.head())  
+
+    df = pd.read_csv(file, encoding="utf-8", sep=";", parse_dates=["DATA"], on_bad_lines='warn')  
+    
+    df.columns = ["DATA_REGISTRO", "REG_ANS", "CD_CONTA_CONTABIL", "DESCRICAO", "VL_SALDO_INICIAL", "VL_SALDO_FINAL"]
+    
+    df.to_sql("despesas_saude", con=engine, if_exists="append", index=False)
+
+print("Importação concluída!")
